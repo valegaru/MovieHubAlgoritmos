a2f7a008d1dce1a2393a8eae3882ed05e8d86d0b
valegaru <valegaru2003@gmail.com>
2024-03-28 18:19:42 -0500

    refractornav

--- src/components/Banner/Nav/Nav.ts
+++ src/components/Banner/Nav/Nav.ts
@@ -15,16 +15,21 @@ class Navbar extends HTMLElement {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
             <header>
-                <div class="search-bar">
+						<nav>
+                <section class="search-bar">
                     <input class="barra" type="text" >
                     <div class="icon">
                         <img src="${SearchIcon}" alt="search logo" draggable="false" />
                     </div>
-                </div>
-                <nav>
-                    <a href="#">sign in/ log in</a>
-                    <a href="#">create account</a>
-                    <a href="#">films</a>
+                </section>
+<section class="pages">
+										<a href="#">films</a> <!-- aparece siempre de numero 1 -->
+										<a href="#">lists</a> <!-- aparece cuando se esta logueado de numero 2 -->
+										<a href="#">profile</a> <!-- aparece cuando se esta logueado de numero 3 -->
+                    <a href="#">log in</a> <!-- aparece cuando NO se esta logueado de numero 2 -->
+                    <a href="#">create account</a> <!-- aparece cuando NO se esta logueado de numero 3 -->
+										</section>
+
                 </nav>
 		    </header>
             `;
