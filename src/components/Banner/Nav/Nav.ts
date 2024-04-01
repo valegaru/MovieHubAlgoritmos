8d86cbf0d035b40ed29f126847895c1b25a2b199
Liziyeah <marianitalizcanor@gmail.com>
2024-04-01 00:05:58 -0500

    feat navbar

--- src/components/Banner/Nav/Nav.ts
+++ src/components/Banner/Nav/Nav.ts
@@ -21,8 +21,23 @@ class Navbar extends HTMLElement {
                     </section>
 										<input class="barra" type="text" placeholder="Enter movie name" >
                 </section>
-<section class="pages">
+										<section class="pages">
 										<a href="#">films</a> <!-- aparece siempre de numero 1 -->
+										<ul class = "menu-desplegable">
+											<li><a href="">Classics</a></li>
+											<li><a href="">Popular</a></li>
+											<li><a href="">On cinema</a></li>
+											<li><a href="">Trending</a></li>
+											<li><a href="">Action</a></li>
+											<li><a href="">Sci-fi</a></li>
+											<li><a href="">Romance</a></li>
+											<li><a href="">Comedy</a></li>
+											<li><a href="">Thriller</a></li>
+											<li><a href="">Drama</a></li>
+											<li><a href="">Animated</a></li>
+											<li><a href="">Documental</a></li>
+											<li><a href="">Psychological</a></li>
+										</ul>
 										<!--<a href="#">lists</a>  aparece cuando se esta logueado de numero 2 -->
 										<!--<a href="#">profile</a>  aparece cuando se esta logueado de numero 3 -->
                     <a href="#">log in</a> <!-- aparece cuando NO se esta logueado de numero 2 -->
