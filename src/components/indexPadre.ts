33a6e99f61214af67db3f0b9f6194632463f1d0c
valegaru <valegaru2003@gmail.com>
2024-04-28 16:37:14 -0500

    refractor export Button Custom in indexPadre

--- src/components/indexPadre.ts
+++ src/components/indexPadre.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from './Banner/Nav/Nav';
 export { default as MovieCard } from './MovieCard/MovieCard';
 export { default as CategorySection } from './filterCategories/filterCategories';
 export { default as AddMoviesModal } from './addMovie/addMovie';
+export {default as ButtonCustom} from './ButtonCustom/ButtonCustom'
