ce7dccffec6c50b897094e9cf660bd7e71db125c
valegaru <valegaru2003@gmail.com>
2024-04-28 16:39:58 -0500

    refractor addMoviesModal

--- src/components/exports.ts
+++ src/components/exports.ts
@@ -2,5 +2,5 @@ export { default as Banner } from './Banner/Banner';
 export { default as Navbar } from './Banner/Nav/Nav';
 export { default as MovieCard } from './MovieCard/MovieCard';
 export { default as CategorySection } from './filterCategories/filterCategories';
-export { default as AddMoviesModal } from './addMovie/addMovie';
+export { default as AddMoviesModal } from './addMoviesModal/addMoviesModal';
 export {default as ButtonCustom} from './ButtonCustom/ButtonCustom'
