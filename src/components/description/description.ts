4d8d1817d9f9d744db2f61bdde2cece8e57932d5
valegaru <valegaru2003@gmail.com>
2024-04-28 19:40:46 -0500

    featDescriptionMovie

--- src/components/description/description.ts
+++ src/components/description/description.ts
@@ -1,1 +1,61 @@
 //En la pantalla de cada peli, es el microcomponente de la descripcion
+//en la pantalla de cada pelicula es la parte del poster con el nombre de la peli abajo y el director y la fecha de lanzamiento
+import styles from './description.css';
+
+export enum AttributeDescriptionMovie {
+	'description' = 'description',
+}
+
+export default class DescriptionMovie extends HTMLElement {
+	description?: string;
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<AttributeDescriptionMovie, null> = {
+			description: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	// Método llamado cuando se cambia un atributo observado
+	attributeChangedCallback(
+		propName: AttributeDescriptionMovie,
+		oldValue: string | undefined,
+		newValue: string | undefined
+	) {
+		// Actualización del valor del atributo en la instancia
+		switch (propName) {
+			default:
+				this[propName] = newValue;
+				break;
+		}
+		// Se vuelve a renderizar el componente
+		this.render();
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		// Verificar si existe el shadowRoot
+		if (this.shadowRoot) {
+			// Se establece la estructura HTML del componente
+			this.shadowRoot.innerHTML = `
+			<section>
+<p>${this.description}</p>
+      </section>
+      `;
+		}
+		// Se crea un elemento <style> para aplicar los estilos CSS
+		const cssDescriptionMovie = this.ownerDocument.createElement('style');
+		cssDescriptionMovie.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssDescriptionMovie);
+	}
+}
+
+customElements.define('my-descriptionmovie', DescriptionMovie);
