1b8049c25983eff9107b27850c4856a5410711fb
valegaru <valegaru2003@gmail.com>
2024-04-28 21:42:30 -0500

    refractor

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -22,7 +22,6 @@ class AppContainer extends HTMLElement {
 			<body>
       <my-banner message="Track films you've watched. Save those you want to see. Tell your friends what's good."
 			buttonlabel="GET AN ACCOUNT, IT'S FREE"></my-banner>
-			//<my-submitandterms btn_text="adios" message='hello'></my-submitandterms>
 			<category-section name="Popular films in the community" category="popular" link="https://myflixerz.to/movie"></category-section>
 				<category-section name="On cinema right now" category="cinema"></category-section>
 				<category-section name="Trending this week" category="trending"></category-section>
