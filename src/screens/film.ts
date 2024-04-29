c5c5dacc57eb17d7b56d42bace7e614505bc2e2a
valegaru <valegaru2003@gmail.com>
2024-04-29 11:11:53 -0500

    featfilmscreenbasic

---
+++ src/screens/film.ts
@@ -0,0 +1,26 @@
+// cada pelicula
+import { addObserver, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+import { movies } from '../services/dataFetch';
+import '../components/exports';
+
+export class FilmPage extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<h1>FilmPage</h1>
+			`;
+		}
+	}
+}
+
+customElements.define('app-filmpage', FilmPage);
