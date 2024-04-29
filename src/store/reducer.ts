981773abdb47bc71eda2b6df7b5fd5645214ccc4
valegaru <valegaru2003@gmail.com>
2024-04-29 09:40:43 -0500

    featreducernavigate

---
+++ src/store/reducer.ts
@@ -0,0 +1,12 @@
+export const reducer = (currentAction: any, currentState: any) => {
+	//Payload: es el valor que queremos cambiar o asignar
+	const { action, payload } = currentAction;
+
+	switch (action) {
+		case 'navigate':
+			currentState.screen = payload;
+			break;
+	}
+
+	return currentState;
+};
