f4a36b22c55714d4812575f3607d2784d3b7fafc
valegaru <valegaru2003@gmail.com>
2024-04-28 21:41:38 -0500

    featScreenLogIn

---
+++ src/screens/login.ts
@@ -0,0 +1,30 @@
+import { movies } from '../services/dataFetch';
+import './components/exports';
+
+class LogIn extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<body>
+			<section>
+     <my-sessionform utitle="LOG IN" password="Password" emailornumber="Mobile number or email"></my-sessionform>
+		 <my-submitandterms btn_text="Continue" message="By using our services you agree to"></my-submitandterms>
+		 </section>
+		 <hr><p>New to this website</p><hr>
+		 <a href="sign in page"><button>Create your account</button></a>
+				</body>
+			`;
+		}
+	}
+}
+
+customElements.define('app-login', LogIn);
