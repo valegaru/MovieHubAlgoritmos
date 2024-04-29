c9a8686c611ce8c776a2680e402c211140787f9f
valegaru <valegaru2003@gmail.com>
2024-04-29 10:56:02 -0500

    featCategoriesScreen

--- src/screens/categories.ts
+++ src/screens/categories.ts
@@ -1,1 +1,26 @@
 //creo que deberia ser una por categoria pero luego resolvemos esta parte de las pantallas
+import { addObserver, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+import { movies } from '../services/dataFetch';
+import '../components/exports';
+
+export class Categories extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<h1>CATEGORIES</h1>
+			`;
+		}
+	}
+}
+
+customElements.define('app-categories', Categories);
