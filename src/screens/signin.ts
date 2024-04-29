c1263288df1258f6885cd15c28d1ed57ec29ee69
valegaru <valegaru2003@gmail.com>
2024-04-28 22:23:22 -0500

    featBtn_typeSignIn

--- src/screens/signin.ts
+++ src/screens/signin.ts
@@ -17,7 +17,7 @@ class SignIn extends HTMLElement {
 			<body>
 			<section>
      <my-sessionform utitle="SIGN IN" email="Email" your_name="Your name" mobile_number="Mobile number" re_password="Re-enter password" password="Password" ></my-sessionform>
-		 <my-submitandterms btn_text="Create account" message="By creating an account, you agree to"></my-submitandterms>
+		 <my-submitandterms btn_text="Create account" message="By creating an account, you agree to" btn_type="submit"></my-submitandterms>
 		 <hr>
 		 <p>Already have an account? <a href="Log in page">Log in</a></p>
 		 </section>
