44212ebe3426d2b60429d5a4d8950ff0e2735cd6
valegaru <valegaru2003@gmail.com>
2024-04-30 16:51:05 -0500

    featscreenNewList

---
+++ src/screens/newlist.ts
@@ -0,0 +1,25 @@
+import { addObserver, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+import { movies } from '../services/dataFetch';
+import '../components/exports';
+
+export class NewList extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<h1>NewList</h1>
+			`;
+		}
+	}
+}
+
+customElements.define('app-newlist', NewList);
