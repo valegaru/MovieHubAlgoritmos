46b7632eadbad56ad155e710260fcc9aa3fb41e8
valegaru <valegaru2003@gmail.com>
2024-05-03 18:09:12 -0500

    firebaseNpmAndImport

---
+++ src/services/firebase.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'; //Importar los modulos
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { collection, addDoc, getDocs, doc, setDoc } from 'firebase/firestore'; //Importar funciones para agregar info a la db
