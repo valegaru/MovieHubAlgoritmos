5d74a7b3abdc570b45e9ccf18aed07fee116a172
valegaru <valegaru2003@gmail.com>
2024-05-07 15:59:39 -0500

    categoryImagefromDashboardDone

--- src/screens/dashboard.ts
+++ src/screens/dashboard.ts
@@ -33,10 +33,10 @@ export class Dashboard extends HTMLElement {
 			<body>
       <my-banner message="Track films you've watched. Save those you want to see. Tell your friends what's good."
 			buttonlabel="GET AN ACCOUNT, IT'S FREE"></my-banner>
-			<category-section name="Popular films in the community" category="popular" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmpvHcS9bCsIq_bYVd5bdKCk2_EamUfe8nlTtHEhlA-g&s"></category-section>
-				<category-section name="On-cinema right now" category="cinema"></category-section>
-				<category-section name="Trending this week" category="trending"></category-section>
-				<category-section name="Classics" category="classics"></category-section>
+			<category-section name="Popular films in the community" category="popular" image="https://media.themoviedb.org/t/p/w533_and_h300_bestv2/s5R6kTMfOxkGit96A8lqcDL4uVk.jpg"></category-section>
+				<category-section name="On-cinema right now" category="cinema" image="https://media.themoviedb.org/t/p/w533_and_h300_bestv2/rron9HAuS9s7zBF8iCX1tsafxUo.jpg"></category-section>
+				<category-section name="Trending this week" category="trending" image="https://media.themoviedb.org/t/p/w533_and_h300_bestv2/iAyr7VEFGJoHDIHIxjEb3TskQW4.jpg"></category-section>
+				<category-section name="Classics" category="classics" image="https://media.themoviedb.org/t/p/w533_and_h300_bestv2/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg"></category-section>
 				<modal-movie></modal-movie>
 				</body>
 			`;
