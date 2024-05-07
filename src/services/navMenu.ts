6fcea48f38e7acf1505217acfc169122b97d7c37
valegaru <valegaru2003@gmail.com>
2024-05-07 12:50:20 -0500

    navMenudata

--- src/services/navMenu.ts
+++ src/services/navMenu.ts
@@ -1,59 +1,73 @@
 export interface DataShapeNavMenu {
 	name: string;
 	category: string;
+	image: string;
 }
 
 export const navMenu: DataShapeNavMenu[] = [
 	{
 		name: 'Classics',
 		category: 'classics',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
 	},
 	{
 		name: 'Popular',
 		category: 'popular',
+		image: '',
 	},
 	{
 		name: 'On cinema',
 		category: 'cinema',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/rron9HAuS9s7zBF8iCX1tsafxUo.jpg',
 	},
 	{
 		name: 'Trending',
 		category: 'trending',
+		image: '',
 	},
 	{
 		name: 'Action',
 		category: 'action',
+		image: '',
 	},
 	{
 		name: 'Sci-fi',
 		category: 'sci-fi',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/cNffd3wkuIYZMelbPRAZ23E5HBT.jpg',
 	},
 	{
 		name: 'Romance',
 		category: 'romance',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/nofXR1TN1vgGjdfnwGQwFaAWBaY.jpg',
 	},
 	{
 		name: 'Comedy',
 		category: 'comedy',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/qdIMHd4sEfJSckfVJfKQvisL02a.jpg',
 	},
 	{
 		name: 'Thriller',
 		category: 'thriller',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/bneUTWFAcVCdsb0O5UwZbJd8xqZ.jpg',
 	},
 	{
 		name: 'Drama',
 		category: 'drama',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/pcWxKfFNCznTKYy0E8M9nG1cwL4.jpg',
 	},
 	{
 		name: 'Animated',
 		category: 'animated',
+		image: 'https://media.themoviedb.org/t/p/w533_and_h300_bestv2/sRvXNDItGlWCqtO3j6wks52FmbD.jpg',
 	},
 	{
 		name: 'Documental',
 		category: 'documental',
+		image: '',
 	},
 	{
 		name: 'Psychological',
 		category: 'psychological',
+		image: '',
 	},
 ];
