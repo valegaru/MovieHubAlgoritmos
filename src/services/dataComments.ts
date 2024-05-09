68415483b7d919f089940613d9207a6a5b34eb8c
Liziyeah <marianitalizcanor@gmail.com>
2024-05-08 20:51:48 -0500

    featdatComments

---
+++ src/services/dataComments.ts
@@ -0,0 +1,34 @@
+export interface DataCommentSection {
+	id: number;
+	name: string;
+	image: string;
+	comment: string;
+}
+
+export const comments: DataCommentSection[] = [
+	{
+		id: 1,
+		name: 'John',
+		image: 'https://i.pinimg.com/564x/72/d6/96/72d69624326a417824f5d96e36ebf4eb.jpg',
+		comment: 'may thy racket chip and shatter',
+	},
+	{
+		id: 2,
+		name: 'sydney',
+		image: 'https://i.pinimg.com/736x/3a/e5/0e/3ae50eb0d74405a95fa5865988b9ba3c.jpg',
+		comment: 'god can thomas brodie sangster have more american roles',
+	},
+	{
+		id: 3,
+		name: 'ccaitlinnm',
+		image: 'https://i.pinimg.com/564x/5f/22/e5/5f22e519b409fe620193ca47f3724b83.jpg',
+		comment:
+			'ngl i thought bubba gumps came before this film and the writers spent like half the movie building up to a punchline',
+	},
+	{
+		id: 4,
+		name: 'DirkH',
+		image: 'https://i.pinimg.com/564x/9b/11/77/9b1177aebfc5f7ea7c3124e30e809970.jpg',
+		comment: 'nobody likes getting the most out of a vowel quite like james stewart',
+	},
+];
