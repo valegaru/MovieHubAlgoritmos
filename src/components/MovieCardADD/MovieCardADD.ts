0a861b8cb7b6da7195746ba969a9d0cd5f3bb48b
valegaru <valegaru2003@gmail.com>
2024-05-09 21:55:06 -0500

    styles MovieCard add

--- src/components/MovieCardADD/MovieCardADD.ts
+++ src/components/MovieCardADD/MovieCardADD.ts
@@ -1,8 +1,7 @@
-//Los posters de pelicula con un mas por encima de newlist
 import styles from './MovieCardADD.css';
 
 export enum AttributeMovieCardAdd {
-	'image' = 'image', //from data
+	'image' = 'image',
 	'uid' = 'uid',
 	'categories' = 'categories',
 	'utitle' = 'utitle',
@@ -27,10 +26,12 @@ export default class MovieCardAdd extends HTMLElement {
 	image_sec?: string;
 	description?: string;
 	catch_phrase?: string;
+	isLiked: boolean;
 
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+		this.isLiked = false; // Inicialmente no está en la lista de favoritos
 	}
 
 	static get observedAttributes() {
@@ -66,17 +67,40 @@ export default class MovieCardAdd extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-			<div class="container" data-uid="${this.uid}" data-image="${this.image}" data-categories="${this.categories}" data-title="${this.utitle}" data-director="${this.director}" data-release="${this.release_date}" data-cast="${this.cast}" data-crew="${this.crew}" data-imgbanner="${this.image_sec}" data-description="${this.description}" data-phrase="${this.catch_phrase}">
-			<img class="poster" src="${this.image}" >
-			<section class="content">
-    <button id ="AddButton">+</button>
-			`;
+                <div class="container" data-uid="${this.uid}" data-image="${this.image}" data-categories="${
+				this.categories
+			}" data-title="${this.utitle}" data-director="${this.director}" data-release="${this.release_date}" data-cast="${
+				this.cast
+			}" data-crew="${this.crew}" data-imgbanner="${this.image_sec}" data-description="${
+				this.description
+			}" data-phrase="${this.catch_phrase}">
+                    <img class="poster" src="${this.image}" >
+                    <section class="content">
+                        <button id="LikeButton">${this.isLiked ? '-' : '+'}</button>
+                    </section>
+                </div>
+            `;
 		}
 
 		// Se crea un elemento <style> para aplicar los estilos CSS
 		const cssMovieCardAdd = this.ownerDocument.createElement('style');
 		cssMovieCardAdd.innerHTML = styles;
 		this.shadowRoot?.appendChild(cssMovieCardAdd);
+
+		// Obtener referencia al botón de like/dislike
+		const likeButton = this.shadowRoot?.querySelector('#LikeButton') as HTMLButtonElement;
+
+		// Agregar listener al botón
+		likeButton.addEventListener('click', () => {
+			// Cambiar el estado de isLiked
+			this.isLiked = !this.isLiked;
+
+			// Cambiar el texto del botón según el estado
+			likeButton.textContent = this.isLiked ? '-' : '+';
+
+			// Realizar aquí las acciones que desees al dar clic al botón
+			// Por ejemplo, agregar o remover de la lista de favoritos, etc.
+		});
 	}
 }
 
