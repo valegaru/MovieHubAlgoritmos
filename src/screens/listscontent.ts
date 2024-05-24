8a67947a006ecb36c12b7f7fbe57adc84a47cb96
valegaru <valegaru2003@gmail.com>
2024-05-23 19:06:55 -0500

    feat screen listcontent

---
+++ src/screens/listscontent.ts
@@ -0,0 +1,30 @@
+import { addObserver, appState, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+import '../components/exports';
+import { Navbar } from '../components/exports';
+import { Banner } from '../components/exports';
+import styles from './newlist.css'; // cambiar
+
+export class ListContent extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = /*html*/ `
+			<my-banner section_title=${appState.currentnewlistname} image=${appState.currentnewlistimage}></my-banner>
+			`;
+		}
+		const cssIndex = this.ownerDocument.createElement('style');
+		cssIndex.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssIndex);
+	}
+}
+
+customElements.define('app-listcontent', ListContent);
